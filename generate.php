c8154e1433f0f9c652f9024746b1f69c510fa26e
Aaron Jorbin <aaron@jorb.in>
2019-09-20 11:10:31 -0400

    initial commit

---
+++ generate.php
@@ -0,0 +1,16 @@
+<?php
+
+$files = range( 1, 100000 );
+$run_file = [ '<?php' ];
+
+foreach( $files as $file ){
+	$dir = '<?php return __DIR__;';
+	$dirname = '<?php return dirname( __FILE__ );';
+	file_put_contents( './script/dir/' . $file . '.php', $dir ); 
+	file_put_contents( './script/dirname/' . $file . '.php', $dirname ); 
+	$run_file[] = 'require( "' . $file . '.php" );';
+}
+
+
+file_put_contents( './script/dir/run.php', implode( $run_file, "\n" ) ) ; 
+file_put_contents( './script/dirname/run.php', implode( $run_file, "\n" ) ) ; 
---
+++ script/dir/15482.php
@@ -0,0 +1,1 @@
+<?php return __DIR__;
